1067d528b8e6017e61517949ed54e6513ffb8324
othmane <lheothmane@gmail.com>
2025-11-04 12:14:22 +0100

    develop login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+];
--- src/app/pages/login/login.component/login.component.ts
+++ src/app/pages/login/login.component/login.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-login.component',
-  imports: [],
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email = '';
+  password = '';
 
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
+
