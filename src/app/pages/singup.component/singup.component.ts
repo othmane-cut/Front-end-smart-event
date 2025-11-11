80aed800a49c4bb7ed86a54da5e84a73bc086a46
othmane <lheothmane@gmail.com>
2025-11-11 23:00:40 +0100

    creat sing up compoent

---
+++ src/app/pages/singup.component/singup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singup.component',
+  imports: [],
+  templateUrl: './singup.component.html',
+  styleUrl: './singup.component.css',
+})
+export class SingupComponent {
+
+}
